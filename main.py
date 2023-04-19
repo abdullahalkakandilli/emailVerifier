19f1d918ccb0e479a03396fdaef39e30a8ecea34
Abdullah Alka Kandilli <alkakandilli@gmail.com>
2023-04-19 16:31:52 +0200

    fix email validation

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def get_values(column_names):
         domain = email.split('@')[1]
         domain_valid = check_email_domain(domain)
         email_valid = validate_email(email)
-        st.write(domain_valid)
+        st.write(email_valid)
         if domain_valid == True and email_valid == True:
             df.loc[index, "Verification"] = "Valid"
 
